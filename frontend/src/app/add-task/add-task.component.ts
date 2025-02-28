8adc83c3e021c0438219a09f3e8264b8ad1574d4
lakmalpasindu <nanayakk-cs18027@stu.kln.ac.lk>
2025-02-28 18:33:25 +0530

    fix: update task issue

--- frontend/src/app/add-task/add-task.component.ts
+++ frontend/src/app/add-task/add-task.component.ts
@@ -120,7 +120,10 @@ export class AddTaskComponent implements OnInit {
   }
 
   public formatDate(date: Date) {
-    return date.toISOString().split('T')[0];
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
 
   public formatTime(time: Date): string {
